a5c814f501f1801f791c03ae0e52aa08dc1b80ef
guru059 <48175517+guru059@users.noreply.github.com>
2022-09-19 17:37:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,20 +2,20 @@ provider "aws" {
     region = "ap-south-1"
 }
 
-resource "aws_vpc" "myvpc1" {
+resource "aws_vpc" "mytestvpc1" {
 
     cidr_block =    "10.0.0.0/16"
 
         tags = {
-        Name = "testvpc"
+        Name = "testvpc1"
     }
 }
 
-resource "aws_vpc" "myvpc2" {
+resource "aws_vpc" "mytestvpc2" {
 
     cidr_block =    "10.0.0.0/16"
 
         tags = {
-        Name = "testvpc"
+        Name = "testvpc2"
     }
 }
